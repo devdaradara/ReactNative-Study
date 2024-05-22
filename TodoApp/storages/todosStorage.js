2f68f60004c0f461435cf65854ef00f15ed2b50e
devdaradara <devdaradara@gmail.com>
2024-05-22 15:47:57 +0900

    TodoApp - FlatList, AsyncStorage

---
+++ TodoApp/storages/todosStorage.js
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-community/async-storage";
+
+const key = "todos";
+
+const todosStorage = {
+  async get() {
+    try {
+      const rawTodos = await AsyncStorage.getItem(key);
+
+      if (!rawTodos) {
+        // 데이터가 없을 때는 사용하지 않음
+        throw new Error("No saved todos");
+      }
+
+      const savedTodos = JSON.parse(rawTodos);
+      return savedTodos;
+    } catch (e) {
+      throw new Error("Failed to load todos");
+    }
+  },
+
+  async set(data) {
+    try {
+      await AsyncStorage.setItem(key, JSON.stringify(data));
+    } catch (e) {
+      throw new Error("Failed to save todos");
+    }
+  }
+};
+
+export default todosStorage;
