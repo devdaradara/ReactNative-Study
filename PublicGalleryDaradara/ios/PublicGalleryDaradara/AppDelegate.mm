b6286ff1bf142f3b8173b972dd2ba4a051349b65
devdaradara <devdaradara@gmail.com>
2024-05-27 15:53:32 +0900

    bug: ios 빌드

--- PublicGalleryDaradara/ios/PublicGalleryDaradara/AppDelegate.mm
+++ PublicGalleryDaradara/ios/PublicGalleryDaradara/AppDelegate.mm
@@ -1,7 +1,8 @@
 #import "AppDelegate.h"
 #import <Firebase.h>
-
+#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
@@ -17,7 +18,7 @@
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"rnFirbaseExample"
+                                                   moduleName:@"rnFirebaseExample"
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
